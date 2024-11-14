df3b69b8de18c1cf02031f134be82171ff32053a
AndrewwIson <125568648+AndrewwIson@users.noreply.github.com>
2024-11-14 17:15:59 +0100

    Twitchweb

---
+++ script.js
@@ -0,0 +1,24 @@
+// Lista degli streamer di cui vuoi mostrare le live
+const STREAMERS = ["sola08_", "IzunaTOP"]; // Inserisci i nomi degli streamer qui
+
+function loadStreamPlayers() {
+  const streamsContainer = document.getElementById('streams');
+  streamsContainer.innerHTML = ''; // Svuota il contenitore
+
+  STREAMERS.forEach(streamer => {
+    const streamElement = document.createElement('div');
+    streamElement.className = 'stream';
+    streamElement.innerHTML = `
+      <h2>${streamer}</h2>
+      <iframe
+        src="https://player.twitch.tv/?channel=${streamer}&parent=tuo-dominio.github.io" 
+        height="400"
+        width="600"
+        allowfullscreen>
+      </iframe>`;
+    streamsContainer.appendChild(streamElement);
+  });
+}
+
+// Carica i player al caricamento della pagina
+loadStreamPlayers();
